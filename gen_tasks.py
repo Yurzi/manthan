fd72723dc7b04289ba0a5db718550b6d8fe9065a
Yurzi <yurzi@foxmail.com>
2023-12-04 00:33:50 +0800

    fix out of index bug

--- gen_tasks.py
+++ gen_tasks.py
@@ -9,6 +9,21 @@ def get_files(base):
                 yield os.path.join(root, file)
 
 
+def has_result(file: str | os.PathLike) -> bool:
+    log_dir = "log"
+    basname = os.path.basename(file).split(".")[0:-1]
+    basname = ".".join(basname)
+
+    for root, dirs, files in os.walk(log_dir):
+        for file in files:
+            if file.endswith(".pkl"):
+                file = file.split(".")[0:-1]
+                file = ".".join(file)
+                if basname == file:
+                    return True
+    return False
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
@@ -23,8 +38,16 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
     fd = open(args.output, "w")
+    total = 0
+    todo = 0
     for file in get_files(args.task_dir):
+        total += 1
+        if has_result(file):
+            continue
+
+        todo += 1
         cmd = format_str + file
         fd.write(cmd + "\n")
 
     fd.close()
+    print(f"Total tasks: {total}, todo: {todo}")
